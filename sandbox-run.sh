f77d5644ad0bba0f5418598462d959a4c3d50fc2
Ivan Derlich <a@ivanderlich.com>
2026-01-22 13:13:45 -0300

    Allow sandobox

--- sandbox-run.sh
+++ sandbox-run.sh
@@ -1,8 +1,7 @@
 docker rm -f sandbox3000 >/dev/null 2>&1 || true
 
-docker create -it --name sandbox3000 -p 3000:3000 node:20-bullseye bash
-docker cp . sandbox3000:/work
+docker create -it --name sandbox3000 -p 3000:3000 -v "$(pwd)":/work node:20-bullseye bash
 
 docker start sandbox3000
-docker exec -it -w /work sandbox3000 bash -lc "npm i && (npm start dev &) && exec bash"
+docker exec -it -w /work sandbox3000 bash -lc "npm i && (npm run dev-webpack &) && exec bash"
 
