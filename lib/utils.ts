bb45b17b562d5d203828a16e7886a091f2a009f6
Gerardo Escalante <gescalanteleal@gmail.com>
2026-01-29 19:11:51 -0300

    Un cambio de prueba

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,7 @@
+/**
+ * Utilidades compartidas (clases CSS, etc.).
+ * DEV: Este archivo se usa en toda la app; no eliminar cn() sin revisar imports.
+ */
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
